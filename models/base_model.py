0da02f4407c33c00370d5324c7b14c4bcdc76d86
Naasor <amofasandra7@gmail.com>
2024-01-13 21:45:39 +0000

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -15,27 +15,21 @@ class BaseModel:
             **kwargs (dict): key pair attribute
         """
         t_format = "%Y-%m-%dT%H:%M:%S.%f"
-
-        if kwargs:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "update_at":
-                    setattr(self, key, datetime.strptime(value, t_format))
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, t_format)
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[key] = value
         else:
-            self.id = str(uuid.uuid4())
-
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        models.storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """current datetime is saved at updated_at"""
         self.updated_at = datetime.now()
-
         models.storage.save()
 
     def to_dict(self):

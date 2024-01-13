eebaccacd5cdf7e5ede27fe0024ed78f5127cdc0
Naasor <amofasandra7@gmail.com>
2024-01-13 20:39:44 +0000

    basemodel

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr.bin.python
+"""__init__ method for model directory"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
---
+++ models/base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Defines base model class"""
+from uuid import uuid4
+from datetime import datetime
+import models
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new BaseModel
+
+        Args:
+            *args: Unused
+            **kwargs (dict): key pair attribute
+        """
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "update_at":
+                    setattr(self, key, datetime.strptime(value, t_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str (uuid.uuid4())
+
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+        models.storage.new(self)
+
+    def save(self):
+        """current datetime is saved at updated_at"""
+        self.updated_at = datetime.now()
+
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns the dictionary of the BaseModel instance"""
+        inst_dict = self__dict__.copy()
+        inst_dict["__class__"] = self.__class__.name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+        return inst_dict
+
+    def __str__(self):
+        """Print string representation of BaseModel inst."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+                                                                                                                                                          52,8          Bot
+

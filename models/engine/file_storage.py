324737ab24a6296e47ab9f57e45b51f3bf3344dc
Naasor <amofasandra7@gmail.com>
2024-01-13 20:38:28 +0000

    basemodel~

---
+++ models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+
+    def save(self):
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(objdict, file)
+
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+               json_load = json.load(file)
+            for key, value in json_load.items():
+                FileStorage.__object[key] = Basemodel(**value)
+        except:
+            pass
